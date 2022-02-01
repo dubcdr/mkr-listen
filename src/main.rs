65d67ea607a20dd4119f5c65af84030a9e2cfac7
Devin Riley <devinriley@gmail.com>
2022-01-31 22:47:13 -0500

    uniswap v2 listener

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::time::Duration;
 
 use anyhow::Ok;
 use ethers::prelude::*;
+use ethers::types::Transaction;
 use ethers::providers::Http;
 
 #[tokio::main]
@@ -13,24 +14,32 @@ async fn main() -> anyhow::Result<()> {
     println!("endpoint ready?: {}", ws.ready());
 
     let provider = Provider::new(ws).interval(Duration::from_millis(2000));
-    let mut stream = provider.watch_blocks().await?.take(5);
+    let mut stream = provider.watch_blocks().await?;
     while let Some(block) = stream.next().await {
-        println!("New block {}", &block);
+        // println!("New block {}", &block);
         let full_block = client
             .get_block_with_txs(block)
             .await?
             .expect("oh shit, block probably hasnt arrived");
 
-        let uniswap_txns: Vec<Transaction> = full_block
+      println!("New block {}", &full_block.hash.unwrap());
+
+        let uniswap_txns: Vec<&Transaction> = full_block
             .transactions
-            .iter_mut()
+            .clone()
+            .iter()
             .filter(|txn| {
                 let is_uniswap_txn: bool = match txn.to {
-                    Some(fromAddress) => {
-                        fromAddress
-                            == "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852"
-                                .parse::<H160>()
-                                .unwrap()
+                    Some(to_address) => {
+                        let uniswap_addr = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+                          .parse::<H160>()
+                          .expect("Can't parse string to H160");
+                        let to_uniswap = to_address
+                            == uniswap_addr;
+                        if to_uniswap {
+                          println!("{} :: Transaction was to {}, uniswap: {}", to_uniswap, to_address, uniswap_addr);
+                        }
+                        to_uniswap
                     }
 
                     None => false,
@@ -39,7 +48,8 @@ async fn main() -> anyhow::Result<()> {
             })
             .collect();
 
-        println!("{}", serde_json::to_string_pretty(&full_block).unwrap());
+
+        // println!("New block {}", serde_json::to_string_pretty(&full_block).unwrap());
     }
     Ok(())
 }
