78098ea0a7e1982c54e7906950c9420261e3976d
Devin Riley <devinriley@gmail.com>
2022-02-03 16:30:13 -0500

    dotenv for infura project id

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,15 @@ pub const UNISWAP_ADDR: &'static str = "0x7a250d5630B4cF539739dF2C5dAcb4c659F248
 
 //Infura Constants
 pub const INFURA_WS_ENDPOINT: &'static str =
-  "wss://mainnet.infura.io/ws/v3/c50426cd378c4a0fb803eff92a4d9aed";
+  "wss://mainnet.infura.io/ws/v3";
 pub const INFURA_HTTP_ENDPOINT: &'static str =
-  "https://mainnet.infura.io/v3/c50426cd378c4a0fb803eff92a4d9aed";
+  "https://mainnet.infura.io/v3";
 
 // Alchemy Constants
 // pub const ALCHEMY_HTTP_ENDPOINT: &'static str =
-//     "https://eth-mainnet.alchemyapi.io/v2/FV4hMUQL6fF4jqAlk317noVRGY4E9MHl";
+//     "https://eth-mainnet.alchemyapi.io/v2";
 // pub const ALCHEMY_WS_ENDPOINT: &'static str =
-//     "wss://eth-mainnet.alchemyapi.io/v2/FV4hMUQL6fF4jqAlk317noVRGY4E9MHl";
+//     "wss://eth-mainnet.alchemyapi.io/v2";
 
 // Geth Constants
 // pub const GETH_HTTP_ENDPOINT: &'static str = "http://localhost:8545";
