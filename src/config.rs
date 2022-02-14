07ca614ff0c3a24f138389c1a1362e9d7bb6daf2
Devin Riley <devinriley@gmail.com>
2022-02-14 11:21:46 -0600

    add time machine cli args

--- src/config.rs
+++ src/config.rs
@@ -16,11 +16,14 @@ pub mod config {
     pub const ALCHEMY_WS_ENDPOINT: &'static str = "wss://eth-mainnet.alchemyapi.io/v2";
 
     // Geth Constants
-    pub const GETH_HTTP_ENDPOINT: &'static str = "http://localhost:8545";
+    pub const GETH_HTTP_ENDPOINT: &'static str = "http:H//localhost:8545";
 
     pub struct UniListenConfig {
         pub ws_url: String,
         pub http_url: String,
+        pub since_block: Option<u64>,
+        pub prev_blocks: Option<u32>,
+        pub watch_blocks: bool,
     }
 
     pub fn get_config() -> UniListenConfig {
@@ -29,17 +32,36 @@ pub mod config {
         let ws_provider_opt = Arg::new("ws-provider-url")
             .long("ws-provider-url")
             .default_value(INFURA_WS_ENDPOINT)
-            .group("ws");
+            .group("provider");
         let http_provider_opt = Arg::new("http-provider-url")
             .default_value("infura")
             .long("http-provider-url")
-            .group("http");
+            .group("provider");
+
+        let since_block_opt = Arg::new("since-block")
+            .long("since-block")
+            .takes_value(true)
+            .group("time-machine");
+        let include_prev_blocks = Arg::new("prev-blocks")
+            .long("include-prev-n-blocks")
+            .takes_value(true)
+            .group("time-machine");
+        let watch_new_blocks = Arg::new("watch-blocks")
+            .long("watch-new-blocks")
+            .default_value("true")
+            .group("time-machine");
 
         let matches = App::new("uni-listen")
             .version("0.1")
             .about("A simple cli app used to watch Uniswap V2 Router")
             .author("Devin Riley")
-            .args([ws_provider_opt, http_provider_opt])
+            .args([
+                ws_provider_opt,
+                http_provider_opt,
+                since_block_opt,
+                include_prev_blocks,
+                watch_new_blocks,
+            ])
             .get_matches();
 
         let http_id: Option<String> = match env::var("HTTP_PROJECT_ID") {
@@ -70,6 +92,41 @@ pub mod config {
             ws_id,
         );
 
-        UniListenConfig { http_url, ws_url }
+        let since_block = match matches.value_of("since-block") {
+            Some(s) => Some(
+                s.parse::<u64>()
+                    .expect("--since-block format must be a u64"),
+            ),
+            None => None,
+        };
+
+        let prev_blocks = match matches.value_of("prev-blocks") {
+            Some(s) => Some(
+                s.parse::<u32>()
+                    .expect("--include-prev-n-blocks format must be a u32"),
+            ),
+            None => None,
+        };
+
+        if since_block.is_some() && prev_blocks.is_some() {
+            panic!("Can't set --since-block and --include-prev-n-blocks");
+        }
+
+        let watch_blocks = match matches.value_of("watch-blocks") {
+            Some(s) => match s.to_lowercase().as_str() {
+                "true" => true,
+                "false" => false,
+                _ => panic!("--watch-blocks must be \"true\" or \"false\""),
+            },
+            None => true,
+        };
+
+        UniListenConfig {
+            watch_blocks,
+            http_url,
+            ws_url,
+            prev_blocks,
+            since_block,
+        }
     }
 }
